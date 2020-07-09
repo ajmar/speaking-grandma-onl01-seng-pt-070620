5e12ea4530cb1c95762f9658823b872238318c76
Alexander Marusin <amarusin18@gmail.com>
2020-07-09 03:35:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,6 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else puts "HUH? SPEAK UP, SONNY!" 
-    
-  end
+  else puts "HUH? SPEAK UP, SONNY!"
 
-  
+  end
